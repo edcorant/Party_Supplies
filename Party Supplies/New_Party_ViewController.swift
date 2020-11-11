c45a275bc6eb9ea252b99c82d989a11f009c5710
Eduardo Antonini <eduardoantonini@icloud.com>
2020-11-10 19:04:10 -0500

    Fixed some UI issues, added a new storyboard for New Party form.

---
+++ Party Supplies/New_Party_ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  New_Party_ViewController.swift
+//  Party Supplies
+//
+//  Created by Eduardo Antonini on 11/10/20.
+//
+
+import UIKit
+
+class New_Party_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func cancel_button(_ sender: UIBarButtonItem) {
+        self.dismiss(animated: true, completion: nil)
+
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
