ed9c8c4a493bee25325603b8d5819c12ff999037
Eduardo Antonini <eduardoantonini@icloud.com>
2020-11-28 00:41:59 -0500

    Tiny UI updates

---
+++ Party Supplies/PartyDetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PartyDetailsViewController.swift
+//  Party Supplies
+//
+//  Created by Eduardo Antonini on 11/27/20.
+//
+
+import UIKit
+
+class PartyDetailsViewController: UIViewController {
+
+    @IBOutlet weak var party_name: UILabel!
+    @IBOutlet weak var party_address: UILabel!
+    @IBOutlet weak var party_time: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func add_supplies(_ sender: Any) {
+        
+        
+        
+    }
+
+}
